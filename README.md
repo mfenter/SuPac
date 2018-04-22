5c4f4f40f268dd6fd0d32499c6bff2922ff4756b
Jared Cain <jared.cain77@gmail.com>
2018-04-22 12:26:25 -0700

    Add merchant app, squarepay,  update readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SuPac App
+
+
+
+### Square SDK Python Install
+
+"You must use the following repo to properly install square"
+
+``$: pip install git+https://github.com/square/connect-python-sdk.git
+``
--- SuPac/settings.py
+++ SuPac/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'inventory.apps.InventoryConfig',
+    'inventory.apps.InventoryConfig'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
