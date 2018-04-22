8f37ce181b4a4f7546b08abe1a02adc8550febcc
Jared Cain <jared.cain77@gmail.com>
2018-04-21 20:35:20 -0700

    Add inventory and models

--- inventory/urls.py
+++ inventory/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 from .views import Index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.default.urls'), name='accounts'),
-    path('inventory/', include('inventory.urls'), name='inventory'),
-    path('', Index.as_view(), name="home")
+    path('', Index.as_view(), name="inventory-index")
 ]
