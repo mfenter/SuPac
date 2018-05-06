eccb3df582cf24f1fd9ed6a060503eddffd99a61
Jared Cain <jaredcain@Jareds-MacBook-Air.local>
2018-05-05 19:19:07 -0700

    WIP make cart in header toggle on logout

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,67 +1,14 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './App.css';
 
 import Header from '../components/Header';
 import PaymentForm from '../merchant/PaymentForm';
 import Cart from '../cart/Cart';
-import { InventoryIndex, PlotView } from '../inventory/Inventory'
+import {InventoryIndex, PlotView} from '../inventory/Inventory'
 
-
-// class App extends Component {
-//
-//     render() {
-//         console.log(this.props); //for test purposes
-//
-//         let component = this.props.component;
-//
-//         if (component == 'home') {
-//             return (
-//                 <div>
-//                     <Header/>
-//                     <h1> This is the main index</h1>
-//                     <p><a href="/accounts/login/">Login</a></p>
-//                     <p><a href="/accounts/register/">Register</a></p>
-//                 </div>
-//             );
-//         }
-//
-//         if (component == 'payment-form') {
-//             return (
-//                 <PaymentForm amount={this.props.amount} />
-//             );
-//         }
-//
-//         if (component == 'cart') {
-//             return (
-//                 <Cart items={this.props.items} total={this.props.total} />
-//             )
-//         }
-//
-//         if(component == 'inventory-index' || component == "celestial_body_view"){
-//             return (
-//                 <InventoryIndex body_list={this.props.body_list} plot_list={this.props.plot_list} />
-//             )
-//         } else {
-//             return (
-//                 <p>Not the right route</p>
-//             );
-//         }
-//
-//         // return (
-//         //   <div className="App">
-//         //     <header className="App-header">
-//         //       <img src={logo} className="App-logo" alt="logo" />
-//         //       <h1 className="App-title">Welcome to SuPac</h1>
-//         //     </header>
-//         //     <p className="App-intro">
-//         //       To get started, edit <code>src/App.js</code> and save to reload.
-//         //     </p>
-//         //   </div>
-//         // );
-//     }
-// }
+import createCartSocket from './websocket';
 
 
 class Home extends Component {
@@ -78,17 +25,81 @@ class Home extends Component {
 
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            "quantity": 0
+        };
+    }
+
+    componentWillMount() {
+        // CHECK FOR USER AND CREATE A CART SOCKET IF USER EXISTS
+        console.log("App mounted");
+    }
+
+    componentDidMount() {
+        console.log(this.props);
+        if (this.props.user !== "undefined") {
+            this.cs = createCartSocket(this.props.user);
+        }
+
+        let selfie = this;
+
+        console.log(this.cs.readyState);
+
+        //TODO: Maybe try to do this.cs.onconnect = func here instead of timeout
+        setTimeout(function () {
+            selfie.cs.send(JSON.stringify({
+                'action': "get"
+            }));
+
+            selfie.cs.onmessage = function (ev) {
+                let data = JSON.parse(ev.data);
+                let qty = data['qty'];
+                selfie.setState({
+                    "quantity": qty
+                });
+            };
+            console.log(selfie.cs.readyState);
+        }, 300);
+    }
+
+    updateCart = (action, plotName) => {
+        //SEND ACTION AND PLOT TO SOCKET AND ...
+        this.cs.send(JSON.stringify({
+            'action': action,
+            'plot': plotName
+        }));
+
+        let selfie = this;
+
+        //RECEIVED FROM THE SERVER SOCKET
+        this.cs.onmessage = function (ev) {
+            let data = JSON.parse(ev.data);
+            let qty = data['qty'];
+            selfie.setState({
+                "quantity": qty
+            });
+            console.log(qty)
+        };
+    };
+
     render() {
         return (
             <Router>
                 <div>
-                    <Header/>
+                    <Header quantity={this.state.quantity} user={this.props.user}/>
                     <Route exact path="/" component={Home}/>
-                    <Route path="/cart/" render={() => <Cart items={this.props.items} total={this.props.total} /> } />
-                    <Route exact path="/inventory/" render={() => <InventoryIndex body_list={this.props.body_list} plot_list={this.props.plot_list} /> } />
-                    <Route exact path="/inventory/celestial_body/:name" render={() => <InventoryIndex body_list={this.props.body_list} plot_list={this.props.plot_list} /> } />
-                    <Route exact path="/inventory/celestial_body/plots/:name" render={() => <PlotView {...this.props} /> } />
-                    <Route path="/merchant/" component={() => <PaymentForm amount={this.props.amount} /> } />
+                    <Route path="/cart/" render={() => <Cart items={this.props.items} total={this.props.total}/>}/>
+                    <Route exact path="/inventory/" render={() => <InventoryIndex body_list={this.props.body_list}
+                                                                                  plot_list={this.props.plot_list}/>}/>
+                    <Route exact path="/inventory/celestial_body/:name"
+                           render={() => <InventoryIndex body_list={this.props.body_list}
+                                                         plot_list={this.props.plot_list}/>}/>
+                    <Route exact path="/inventory/celestial_body/plots/:name"
+                           render={() => <PlotView {...this.props} updateCart={this.updateCart}
+                                                   qty={this.state.quantity}/>}/>
+                    <Route path="/merchant/" render={() => <PaymentForm amount={this.props.amount}/>}/>
                 </div>
             </Router>
         )
--- frontend/src/app/websocket.js
+++ frontend/src/app/websocket.js
@@ -4,18 +4,6 @@ function createCartSocket(cartUsername) {
         'ws://' + window.location.host + '/ws/cart/' + cartUsername + '/'
     );
 
-    cartSocket.onmessage = function(ev) {
-        let data = JSON.parse(ev.data);
-        let message = data['message'];
-        console.log(message)
-    };
-
-    cartSocket.onopen = function() {
-        cartSocket.send(JSON.stringify({
-            "message": "this is loaded"
-        }));
-    };
-
     cartSocket.onclose = function() {
         console.error("Cart Socket closed unexpectedly");
     };
